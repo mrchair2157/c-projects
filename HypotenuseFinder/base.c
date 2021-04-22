22676e2f20ef2c0421d0dae616c1197ba1dffcfe
mrchair2157 <64299722+mrchair2157@users.noreply.github.com>
2021-04-22 00:40:18 +0000

    Add files via upload

--- HypotenuseFinder/base.c
+++ HypotenuseFinder/base.c
@@ -7,16 +7,21 @@ int main()
 	double a;
 	double b;
 	double c;
+
 	printf("this program will find a missing length in a right triangle\n ");
 	printf("enter you first value: ");
 	scanf("%lf",&a);
+
 	printf("enter you second value: ");
 	scanf("%lf",&b);
+
 	printf("if a = %lf and b = %lf ",a,b);
+
 	a = pow(a,2);
 	b = pow(b,2);
 	c = a + b;
 	c = sqrt(c);
+
 	printf("then c = %lf",c);
 return 0;
 }
